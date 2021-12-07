c023d2edacc6f9d490f6a3a2e79f0622988286f5
dubisdev <dubisdev@gmail.com>
2021-12-07 11:24:10 +0100

    generate static

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
-);
+import { hydrate, render } from "react-dom";
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+	hydrate(<App />, rootElement);
+} else {
+	render(<App />, rootElement);
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
