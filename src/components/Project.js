00392943ff825afb3510553a63579ebc9b0f0a8c
dubisdev <dubisdev@gmail.com>
2022-01-18 11:45:56 +0100

    target blank in projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,11 +9,14 @@ const Project = ({ name, url, imgSrc, tech }) => {
 		return (
 			<>
 				<a
+					target="_blank"
+					rel="noopener noreferrer"
 					href={url}
 					className={`relative basis-[450px] rounded-[5px] h-52 m-[0.5%] bg-cover`}
 					style={{ backgroundImage: `url("${imgSrc}")` }}
 					onMouseOver={() => setIsHover(true)}
-					onMouseLeave={() => setIsHover(false)}>
+					onMouseLeave={() => setIsHover(false)}
+				>
 					<div className="absolute flex flex-col top-0 right-0 bottom-0 left-0 items-center justify-center opacity-100 p-4 backdrop-brightness-50">
 						<span className="text-xl">{name}</span>
 
@@ -22,7 +25,8 @@ const Project = ({ name, url, imgSrc, tech }) => {
 								return (
 									<span
 										className="rounded-[5px] p-[5px] bg-black text-[#be99ff] m-[15px]"
-										key={index}>
+										key={index}
+									>
 										{t}
 									</span>
 								);
@@ -36,11 +40,14 @@ const Project = ({ name, url, imgSrc, tech }) => {
 	return (
 		<>
 			<a
+				target="_blank"
+				rel="noopener noreferrer"
 				href={url}
 				className={`relative basis-[450px] rounded-[5px] h-52 m-[0.5%] bg-cover`}
 				style={{ backgroundImage: `url("${imgSrc}")` }}
 				onMouseOver={() => setIsHover(true)}
-				onMouseLeave={() => setIsHover(false)}>
+				onMouseLeave={() => setIsHover(false)}
+			>
 				<div className="absolute flex flex-col top-0 right-0 bottom-0 left-0 items-center justify-center opacity-100 p-4 backdrop-brightness-[0.60] hover:backdrop-brightness-50">
 					<span className="text-xl">{name}</span>
 					{isHover && (
@@ -49,7 +56,8 @@ const Project = ({ name, url, imgSrc, tech }) => {
 								return (
 									<span
 										className="rounded-[5px] p-[5px] bg-black text-[#be99ff] m-[15px]"
-										key={index}>
+										key={index}
+									>
 										{t}
 									</span>
 								);
