a3187c8672f12b35f524be2c147931b306a5c456
dubisdev <dubisdev@gmail.com>
2022-01-18 11:37:29 +0100

    redirect to home

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import "./index.css";
 import MyInfo from "./components/Info";
 import MyProjects from "./components/Projects";
-import { Route } from "wouter";
+import { Route, Redirect } from "wouter";
 
 function App() {
 	return (
-		<Route path="/">
-			<div className="bg-[#181818] min-h-screen p-4">
-				<div className="border-4 border-[#4c00cf] rounded-[15px] p-2 min-h-fit ">
-					<main className="block sm:flex sm:flex-row">
-						<MyInfo />
-						<MyProjects />
-					</main>
+		<>
+			<Route path="/">
+				<div className="bg-[#181818] min-h-screen p-4">
+					<div className="border-4 border-[#4c00cf] rounded-[15px] p-2 min-h-fit ">
+						<main className="block sm:flex sm:flex-row">
+							<MyInfo />
+							<MyProjects />
+						</main>
+					</div>
 				</div>
-			</div>
-		</Route>
+			</Route>
+			<Redirect to="/" />
+		</>
 	);
 }
 
